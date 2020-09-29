cab5a9aae5f441bd993f22665f9d56bfa4ce76b0
imad17748 <syedprogrammer@yahoo.com>
2020-09-29 17:29:39 +0530

    Image Comparer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 import ImageZoom from "./components/ImageZoom";
 import ImageCompare from "./components/ImageCompare";
@@ -6,10 +6,23 @@ import ImageCompare from "./components/ImageCompare";
 import "./App.css";
 
 function App() {
+  const [showImageComparer, setImageComparer] = useState(true);
+
   return (
     <Fragment>
+      <p className="hey">Hey</p>
       <ImageZoom />
-      <ImageCompare />
+      <p className="hey">Hey</p>
+      <button
+        onClick={() => {
+          setImageComparer(!showImageComparer);
+        }}
+      >
+        Image Comparer
+      </button>
+      {showImageComparer && <ImageCompare />}
+
+      <p className="hey">Hey</p>
     </Fragment>
   );
 }
--- src/components/ImageCompare.js
+++ src/components/ImageCompare.js
@@ -1,23 +1,109 @@
-import React, { Fragment, useRef, useState } from "react";
+import React, { Fragment, useRef, useState, useEffect } from "react";
 
-import sceneryImg from "../images/beautiful-scenery.jpg";
+import cx from "classnames";
+import imageCompareStyles from "./ImageCompare.module.css";
 
-import ImageCompareStyles from "./ImageCompare.module.css";
-let imageWidth = 466;
-let imageHeight = 368;
+import forestImg from "../images/img_forest.jpg";
+import snowImg from "../images/img_snow.jpg";
 
+let imageWidth = 300;
+let imageHeight = 200;
+let sliderWidth = 40;
+let sliderHeight = sliderWidth;
+
+//let sliderClicked = false;
 function ImageCompare() {
-  const imageRef = useRef(null);
+  const overlayRef = useRef(null);
 
+  const sliderClicked = useRef(false);
+
+  const [overlayWidth, setOverlayWidth] = useState(imageWidth / 2);
+
+  useEffect(() => {
+    const slideMove = (e) => {
+      //console.log("move");
+      var pos;
+
+      if (!sliderClicked.current) return;
+      /*get the cursor's x position:*/
+      pos = getCursorPos(e);
+      /*prevent the slider from being positioned outside the image:*/
+      let overlayWidth = pos;
+      if (overlayWidth < 0) overlayWidth = 0;
+      if (overlayWidth > imageWidth) overlayWidth = imageWidth;
+      /*execute a function that will resize the overlay image according to the cursor:*/
+
+      setOverlayWidth(overlayWidth);
+    };
+
+    window.addEventListener("mouseup", slideFinish);
+    window.addEventListener("mousemove", slideMove);
+    return () => {
+      window.removeEventListener("mouseup", slideFinish);
+      window.removeEventListener("mousemove", slideMove);
+    };
+  }, []);
+
+  const slideReady = (e) => {
+    e.preventDefault();
+
+    //setSliderState((p) => ({ ...p, sliderClicked: true }));
+    sliderClicked.current = true;
+  };
+  const slideFinish = (e) => {
+    e.preventDefault();
+
+    //setSliderState((p) => ({ ...p, sliderClicked: false }));
+    sliderClicked.current = false;
+  };
+
+  function getCursorPos(e) {
+    var a,
+      x = 0;
+    //e = e || window.event;
+    /*get the x positions of the image:*/
+    a = overlayRef.current.getBoundingClientRect();
+    /*calculate the cursor's x coordinate, relative to the image:*/
+    x = e.pageX - a.left;
+    /*consider any page scrolling:*/
+    x = x - window.pageXOffset;
+    return x;
+  }
   return (
     <Fragment>
-      <div className="img-comp-container">
-        <div className="img-comp-img">
-          <img src="img_snow.jpg" width="300" height="200"></img>
+      <div
+        className={imageCompareStyles.container}
+        style={{ height: imageHeight }}
+      >
+        <div className={imageCompareStyles.image}>
+          <img
+            src={snowImg}
+            width={imageWidth}
+            height={imageHeight}
+            alt=""
+          ></img>
         </div>
-        <div className="img-comp-slider"></div>
-        <div className="img-comp-img img-comp-overlay">
-          <img src="img_forest.jpg" width="300" height="200"></img>
+        <div
+          className={imageCompareStyles.slider}
+          style={{
+            width: sliderWidth,
+            height: sliderHeight,
+            top: `${imageHeight / 2 - sliderHeight / 2}px`,
+            left: `${overlayWidth - sliderWidth / 2}px`,
+          }}
+          onMouseDown={slideReady}
+        ></div>
+        <div
+          className={cx(imageCompareStyles.image, imageCompareStyles.overlay)}
+          style={{ width: overlayWidth }}
+          ref={overlayRef}
+        >
+          <img
+            src={forestImg}
+            width={imageWidth}
+            height={imageHeight}
+            alt=""
+          ></img>
         </div>
       </div>
     </Fragment>
